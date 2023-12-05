8a95cb1777ea50353ca10ce2c226c918290b9522
Mateo.Romero <Mateo.Romero@carvajal.com>
2023-12-05 14:10:18 -0500

    lambda historical allied : se modifica la consulta a la nueva tabla se elimina recorrido de dias se cambia estadosa

--- dev-allied-scheduler-historical-lambda.py
+++ dev-allied-scheduler-historical-lambda.py
@@ -13,42 +13,32 @@ def lambda_handler(event, context):
         sqs_client = boto3.client("sqs")
         
         '''Obtenemos los registron de las programaciones de historicos aliados que estan en estado pendiente ''' 
-        historical_query_statement = ''' SELECT * FROM conf_allied WHERE begins_with("sk", 'id_request#') and state = 'PENDIENTE' '''
+        historical_query_statement = ''' SELECT * FROM aud_load_external_files WHERE process_type = 'historical' and state = 'PENDIENTE' '''
         
         dict_historical_response_query = dynamodb_client.execute_statement(Statement=historical_query_statement)
         dict_historical_response_query = dict_historical_response_query['Items']
         
-        for historical_message in dict_historical_response_query:
-            historical_message["type_process"] = {"S": "historical"}
-            print(historical_message)
+        for historical_reload_request in dict_historical_response_query:
+            print(historical_reload_request)
             '''con los datos del registro de programacion historical generamos el pk y sk para hacer 
             la query que nos permite obtener el path del aliado para enviar los historicos '''
-            pk = 'country#' + historical_message["country"]['S'] + '#id_allied#' + historical_message["id_allied"]['S']
-            sk = 'ean_provider#' + historical_message["ean_provider"]['S'] + '#ean_trader#' + historical_message["ean_trader"]['S']
+            pk = 'country#' + historical_reload_request["country"]['S'] + '#id_allied#' + historical_reload_request["id_allied"]['S']
+            sk = 'ean_provider#' + historical_reload_request["ean_provider"]['S'] + '#ean_trader#' + historical_reload_request["ean_trader"]['S']
 
             historical_path_query_statement = ''' SELECT * FROM conf_allied
                         WHERE pk = '{0}' and sk = '{1}' '''.format(pk, sk)
-            
             historical_path_response_query = dynamodb_client.execute_statement(Statement=historical_path_query_statement)
-            print(historical_path_response_query)
             historical_path_response_query = historical_path_response_query['Items'][0]['historic_path']['S']
-            historical_message["historic_path"] = {"S": historical_path_response_query}
             
-            '''convertimos el atributo star_date y end_date para poder recorrer los dias correspondientes a esas dos fechas
-            por cada dia agregamos el atributo sale_date y enviamos el registro a la cola 
-            '''
-            start_date = datetime.strptime(historical_message["start_date"]["S"][:10], "%Y-%m-%d")
-            end_date = datetime.strptime(historical_message["end_date"]["S"][:10], "%Y-%m-%d")
-
-            while start_date <= end_date:
-                print(start_date)
-                historical_message["sale_date"] = {"S": start_date.strftime("%Y%m%d")}
-                start_date += timedelta(days=1)
-                sqs_client.send_message(
-                    QueueUrl=os.environ['sqs'],
-                    MessageBody=json.dumps(historical_message)
-                )
-  
+            
+            historical_reload_request["historic_path"] = {"S": historical_path_response_query}
+            
+            sqs_client.send_message(
+                QueueUrl=os.environ['sqs'],
+                MessageBody=json.dumps(historical_reload_request)
+            )
+            
+            change_state_dynamo(historical_reload_request,'PROCESANDO')
 
         return {'statusCode': 200}
     
@@ -57,3 +47,21 @@ def lambda_handler(event, context):
         e insertarlo en la tabla de dynamodb llamada aud_services_errors'''
         catch_error(traceback, context, dateutil, datetime, boto3)
         raise
+    
+    
+
+def change_state_dynamo(item,state):
+    DYNAMODB_CLIENT = boto3.client("dynamodb")
+
+    pk = item['pk']['S'] 
+    sk = item['sk']['S']
+    
+    response = DYNAMODB_CLIENT.update_item(
+        TableName='conf_allied',
+        Key={'pk': {'S': pk},'sk': {'S': sk}},
+        UpdateExpression='SET #s = :val1',
+        ExpressionAttributeNames={'#s': 'state'},
+        ExpressionAttributeValues={':val1': {'S': state }}
+    )
+    
+    return response
